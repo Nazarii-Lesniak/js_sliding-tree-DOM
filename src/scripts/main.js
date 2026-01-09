26667bc7d2acc7b4f7b352b85f14e97a96275beb
Nazarii Lesniak <naziksay@gmail.com>
2026-01-09 09:44:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const typeOfProduct = document.querySelectorAll('li');
+
+typeOfProduct.forEach((list) => {
+  const span = document.createElement('span');
+
+  list.prepend(span);
+  span.append(span.nextSibling);
+});
+
+const mainUnorderedList = document.querySelector('.tree');
+
+mainUnorderedList.addEventListener('click', (eventClick) => {
+  eventClick.preventDefault();
+
+  if (eventClick.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const nestedList = eventClick.target.parentNode.querySelector('ul');
+
+  nestedList.hidden = !nestedList.hidden;
+});
